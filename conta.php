312d221ba61af3cce1f933c555025fb536904c15
jesusnicollas <souza.nicollas124@icloud.com>
2022-11-04 01:56:39 -0300

    criado novos metodos, incluido o metodo transferir

--- conta.php
+++ conta.php
@@ -16,7 +16,27 @@
         $this->saldo -= $valorSaque;
          
     }
+    
+    public function valorDeposito(float $valorDeposito): void
+    {
+        if ($valorDeposito < 0) {
+            echo "Transaçaõ de valor ${valorDeposito}R$ não autorizada";
+            return;
+        }
+        $this->saldo += $valorDeposito; 
+       
     }
+    public function transferir(float $valorAtransferir, conta $contaDestino): void
+    {
+        if ($valorAtransferir > $this->saldo) {
+            echo "Transferencia superior ao saldo em conta de: ${valorAtransferir}R$ para transferencia";
+            return;
+        }
+        $this->saque($valorAtransferir);
+        $contaDestino->valorDeposito($valorAtransferir);
+
+    }
+}
     ;
 //require 'conta.php';
 ?>
