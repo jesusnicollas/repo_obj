6a506a8e6a2d3101eb16c9143b5a847b19200d0c
jesusnicollas <souza.nicollas124@icloud.com>
2022-11-04 01:57:23 -0300

    utilizando transferencia por referencia OBJ

--- acoes.php
+++ acoes.php
@@ -6,5 +6,12 @@
     $usuarioUm->saldo = 10000; 
     $usuarioUm->nomeTitular = 'Nicollas de jesus';
     $usuarioUm->cpfTitular ='111.111.005-00';
-    $usuarioUm->saque(10);
-    var_dump($usuarioUm);
+   // $usuarioUm->saque(10);
+
+    $usuarioDois = new conta;
+    $usuarioDois->saldo = 100000; 
+    $usuarioDois->nomeTitular = 'Italo de jesus';
+    $usuarioDois->cpfTitular ='444.444.444-10';
+    $usuarioDois->transferir(5000, $usuarioUm);
+    var_dump( $usuarioDois);
+    var_dump($usuarioUm);  
