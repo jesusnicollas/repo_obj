4a8cb7a443c1732f56c530bd365a985f523c3dfe
jesusnicollas <souza.nicollas124@icloud.com>
2022-12-01 01:49:27 -0300

    Foi implementado o primeiro metodo de saque para o usuário

--- Conta.php
+++ Conta.php
@@ -4,23 +4,29 @@ class Conta
 {
     public string $cpf;
     public string $nome; 
-    private string $user;
-    
-    function __construct ($cpf, $nome, $user) {
-        if ($this->nome < 3){ 
-            echo "Necessário mais de 3 caracteres";
-        exit();
-    
-    }
-        else {$this->nome = $nome
-        ;}
-    }
-    private function exibeNome($user){
-        echo $this->$user        
+    public float $saldo;
+    public function sacaValor ($valorAsacar) {
+        if ($valorAsacar > $this->saldo) {
+            echo "O valor a sacar é maior que o saldo em  conta"
+        ;} else {
+    $this->saldo -= $valorAsacar;
+    }}
+}   // 
+   // function __construct ($cpf, $nome, $user) {
+   //     if ($this->nome < 3){ 
+   //         echo "Necessário mais de 3 caracteres";
+   //     exit();
+   // 
+   // }
+   //     else {$this->nome = $nome
+   //     ;}
+   // }
+   // private function exibeNome($user){
+   //     echo $this->$user        
+//
+//
+   //     ;}
+   // }
 
-
-        ;}
-    }
-
-$conta = new Conta('111.111.005-17', 'Nico', 'jesusateudispor');
-var_dump($conta);
+//$conta = new Conta('111.111.005-17', 'Nico', 'jesusateudispor');
+//var_dump($conta);
