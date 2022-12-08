edffde354504cd1ff01de1865c8feff7e1d8db3d
jesusnicollas <souza.nicollas124@icloud.com>
2022-12-08 01:12:07 -0300

    Implementado metodo transfereSaldo e necessário correção dos metodos do nome

--- Conta.php
+++ Conta.php
@@ -3,20 +3,40 @@
 class Conta 
 {
     public string $cpf;
-    public string $nome; 
-    private float $saldo = 0;
-    public function sacaValor ($valorAsacar) {
+    public string $nome = 'Nicollas'; 
+    public float $saldo = 0;
+    public function sacaValor ($valorAsacar)  :void {
         if ($valorAsacar > $this->saldo) {
-            echo "O valor a sacar é maior que o saldo em  conta"
-        ;} else {
+            echo "O valor a sacar é maior que o saldo em  conta";
+        return;
+        }
     $this->saldo -= $valorAsacar;
-    }}
-    public function depositaValor($valorAdepositar) {
+    }
+    public function depositaValor($valorAdepositar) :void {
         if ($valorAdepositar <= 0) {
             echo 'Valor deve ser maior que 0';
-        } else {  
-        $this->saldo += $valorAdepositar;    
+        return;}  
+    $this->saldo += $valorAdepositar;
+    }
+    public function transfereSaldo($valorAdepositar, $contaDestino) :void {
+        if ($this->saldo < $valorAdepositar) {
+        echo "Você não tem saldo para realizar essa transação";
+        return;
         }
+    $this->sacaValor($valorAdepositar);
+    $contaDestino->depositaValor($valorAdepositar);
+    }
+    private function exibeNome($nome) :void {
+    $this->nome = $nome;
+    echo "$nome";
+    }
+    public function defineNome($nome) {
+        if (strlen($nome < 3)) {
+        echo "Digite o nome completo";
+        return;    
+        }
+    $this->nome = $nome;
+    echo "$nome cadastrado com sucesso";
     }
 }   //  
    // function __construct ($cpf, $nome, $user) {
